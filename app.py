72799cf9716eb0c2fa7cdc757ca771c0aaad06cb
JouCode <salazarjosue2604@gmail.com>
2023-12-26 14:21:51 -0500

    Api Backend Address

--- app.py
+++ app.py
@@ -1,15 +1,16 @@
-from flask import Flask
-from flask_cors import CORS
-from upload_shapefile_pg.infrastructure.driving_adapter.api_rest.routes import api_routes_bp as upload_shapefile_pg
-from search_address.infrastructure.driving_adapter.api_rest.routes import api_routes_bp as search_address
 from public.scripts import create_file_hdf5_address_street
 from dotenv import load_dotenv
 import os
-
 load_dotenv()
 
 if not os.path.exists(os.getenv("ROUTE_FILE_DATA_ADDRESS_STREET_HDF5")):
     create_file_hdf5_address_street(os.getenv("ROUTE_FILE_DATA_ADDRESS_STREET_PARQUET"), os.getenv("ROUTE_FILE_DATA_ADDRESS_STREET_HDF5"))
+
+
+from flask import Flask
+from flask_cors import CORS
+from upload_shapefile_pg.infrastructure.driving_adapter.api_rest.routes import api_routes_bp as upload_shapefile_pg
+from search_address.infrastructure.driving_adapter.api_rest.routes import api_routes_bp as search_address
 
 app = Flask(__name__)
 CORS(app)
